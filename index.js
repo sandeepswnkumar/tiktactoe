a5b54e3dad4926461a352d74a2dbcd3b8507dedb
SANDEEP KUMAR GUPTA <sandeepswnkumar@gmail.com>
2022-04-17 20:36:43 +0530

    upadate index.js

--- index.js
+++ index.js
@@ -35,11 +35,11 @@ const kik = () => {
     if (tikToe === "X") {
         tikToe = "O"
         tik.innerText = "O"
-        player.style.color = "red"
+        player.style.color = "#07215f"
     } else {
         tikToe = "X"
         tik.innerText = "X"
-        player.style.color = "#2ecc71"
+        player.style.color = "#ff0000"
     }
 }
 
@@ -58,9 +58,141 @@ showModal.addEventListener('click', () => {
 //Results
 boxItam.addEventListener('click', () => {
     if (box1.innerText !== "" && box2.innerText !== "" && box3.innerText !== "" && box4.innerText !== "" && box5.innerText !== "" && box6.innerText !== "" && box7.innerText !== "" && box8.innerText !== "" && box9.innerText !== "") {
-        winPlayer.innerHTML = "No one WIN";
-        greet.innerHTML = "Oops";
-        showModal.style.display = 'flex';
+        
+        if ((box1.innerText === "X" && box2.innerText === "X" && box3.innerText === "X") || (box1.innerText === "O" && box2.innerText === "O" && box3.innerText === "O")) {
+            if (box1.innerText === "X" && box2.innerText === "X" && box3.innerText === "X") {
+
+                winPlayer.innerHTML = "Player X WIN";
+                greet.innerHTML = "Congratulations";
+                showModal.style.display = 'flex';
+
+            }
+            if (box1.innerText === "O" && box2.innerText === "O" && box3.innerText === "O") {
+
+                winPlayer.innerHTML = "Player O WIN";
+                greet.innerHTML = "Congratulations";
+                showModal.style.display = 'flex';
+
+            }
+
+        }
+        else if ((box4.innerText === "X" && box5.innerText === "X" && box6.innerText === "X") || (box4.innerText === "O" && box5.innerText === "O" && box6.innerText === "O")) {
+            if (box4.innerText === "X" && box5.innerText === "X" && box6.innerText === "X") {
+                winPlayer.innerHTML = "Player X WIN";
+                greet.innerHTML = "Congratulations";
+                showModal.style.display = 'flex';
+            }
+            if (box4.innerText === "O" && box5.innerText === "O" && box6.innerText === "O") {
+                winPlayer.innerHTML = "Player O WIN";
+                greet.innerHTML = "Congratulations";
+                showModal.style.display = 'flex';
+            }
+
+        }
+        else if ((box4.innerText === "X" && box5.innerText === "X" && box6.innerText === "X") || (box4.innerText === "O" && box5.innerText === "O" && box6.innerText === "O")) {
+            if (box4.innerText === "X" && box5.innerText === "X" && box6.innerText === "X") {
+
+                winPlayer.innerHTML = "Player X WIN";
+                greet.innerHTML = "Congratulations";
+                showModal.style.display = 'flex';
+
+            }
+
+
+            if (box4.innerText === "O" && box5.innerText === "O" && box6.innerText === "O") {
+                winPlayer.innerHTML = "Player O WIN";
+                greet.innerHTML = "Congratulations";
+                showModal.style.display = 'flex';
+            }
+
+        }
+        else if ((box7.innerText === "X" && box8.innerText === "X" && box9.innerText === "X") || (box7.innerText === "O" && box8.innerText === "O" && box9.innerText === "O")) {
+            if (box7.innerText === "X" && box8.innerText === "X" && box9.innerText === "X") {
+                winPlayer.innerHTML = "Player X WIN";
+                greet.innerHTML = "Congratulations";
+                showModal.style.display = 'flex';
+            }
+
+            if (box7.innerText === "O" && box8.innerText === "O" && box9.innerText === "O") {
+                winPlayer.innerHTML = "Player O WIN";
+                greet.innerHTML = "Congratulations";
+                showModal.style.display = 'flex';
+
+            }
+
+        }
+        else if ((box1.innerText === "X" && box4.innerText === "X" && box7.innerText === "X") || (box1.innerText === "O" && box4.innerText === "O" && box7.innerText === "O")) {
+            if (box1.innerText === "X" && box4.innerText === "X" && box7.innerText === "X") {
+                winPlayer.innerHTML = "Player X WIN";
+                greet.innerHTML = "Congratulations";
+                showModal.style.display = 'flex';
+            }
+            if (box1.innerText === "O" && box4.innerText === "O" && box7.innerText === "O") {
+                winPlayer.innerHTML = "Player O WIN";
+                greet.innerHTML = "Congratulations";
+                showModal.style.display = 'flex';
+            }
+
+        }
+        else if ((box2.innerText === "X" && box5.innerText === "X" && box8.innerText === "X") || (box2.innerText === "O" && box5.innerText === "O" && box8.innerText === "O")) {
+            if (box2.innerText === "X" && box5.innerText === "X" && box8.innerText === "X") {
+                winPlayer.innerHTML = "Player X WIN";
+                greet.innerHTML = "Congratulations";
+                showModal.style.display = 'flex';
+            }
+            if (box2.innerText === "O" && box5.innerText === "O" && box8.innerText === "O") {
+                winPlayer.innerHTML = "Player O WIN";
+                greet.innerHTML = "Congratulations";
+                showModal.style.display = 'flex';
+            }
+        }
+        else if ((box3.innerText === "X" && box6.innerText === "X" && box9.innerText === "X") || (box3.innerText === "O" && box6.innerText === "O" && box9.innerText === "O")) {
+            if (box3.innerText === "X" && box6.innerText === "X" && box9.innerText === "X") {
+                winPlayer.innerHTML = "Player X WIN";
+                greet.innerHTML = "Congratulations";
+                showModal.style.display = 'flex';
+            }
+            if (box3.innerText === "O" && box6.innerText === "O" && box9.innerText === "O") {
+                winPlayer.innerHTML = "Player O WIN";
+                greet.innerHTML = "Congratulations";
+                showModal.style.display = 'flex';
+            }
+
+        }
+        else if ((box1.innerText === "X" && box5.innerText === "X" && box9.innerText === "X") || (box1.innerText === "O" && box5.innerText === "O" && box9.innerText === "O")) {
+            if (box1.innerText === "X" && box5.innerText === "X" && box9.innerText === "X") {
+                winPlayer.innerHTML = "Player X WIN";
+                greet.innerHTML = "Congratulations";
+                showModal.style.display = 'flex';
+            }
+            if (box1.innerText === "O" && box5.innerText === "O" && box9.innerText === "O") {
+                winPlayer.innerHTML = "Player O WIN";
+                greet.innerHTML = "Congratulations";
+                showModal.style.display = 'flex';
+            }
+
+        }
+        else if ((box3.innerText === "X" && box5.innerText === "X" && box7.innerText === "X") || (box3.innerText === "O" && box5.innerText === "O" && box7.innerText === "O")) {
+            if (box3.innerText === "X" && box5.innerText === "X" && box7.innerText === "X") {
+                winPlayer.innerHTML = "Player X WIN";
+                greet.innerHTML = "Congratulations";
+                showModal.style.display = 'flex';
+            }
+            if (box3.innerText === "O" && box5.innerText === "O" && box7.innerText === "O") {
+                winPlayer.innerHTML = "Player O WIN";
+                greet.innerHTML = "Congratulations";
+                showModal.style.display = 'flex';
+            }
+
+        }else{
+            winPlayer.innerHTML = "No one WIN";
+            greet.innerHTML = "Oops";
+            showModal.style.display = 'flex';
+        }
+        
+        
+        
+        
     }
     else {
         if ((box1.innerText === "X" && box2.innerText === "X" && box3.innerText === "X") || (box1.innerText === "O" && box2.innerText === "O" && box3.innerText === "O")) {
@@ -208,10 +340,10 @@ box1.addEventListener('click', () => {
         box1.innerHTML = `<div class="active disable">${tikToe}</div>`;
         temp = tikToe;
         if (box1.innerText === "X") {
-            box1.style.color = "yellow"
+            box1.style.color = "#ff0000"
         }
         if (box1.innerText === "O") {
-            box1.style.color = "red"
+            box1.style.color = "#07215f"
         }
         kik();
     }
@@ -234,10 +366,10 @@ box2.addEventListener('click', (e) => {
         temp = tikToe;
         
         if (box2.innerText === "X") {
-            box2.style.color = "yellow"
+            box2.style.color = "#ff0000"
         }
         if (box2.innerText === "O") {
-            box2.style.color = "red"
+            box2.style.color = "#07215f"
         }
         kik();
     }
@@ -255,10 +387,10 @@ box3.addEventListener('click', (e) => {
         box3.innerHTML = `<div class="active disable">${tikToe}</div>`;
         temp = tikToe;
         if (box3.innerText === "X") {
-            box3.style.color = "yellow"
+            box3.style.color = "#ff0000"
         }
         if (box3.innerText === "O") {
-            box3.style.color = "red"
+            box3.style.color = "#07215f"
         }
         kik();
     }
@@ -276,10 +408,10 @@ box4.addEventListener('click', () => {
         box4.innerHTML = `<div class="active disable">${tikToe}</div>`;
         temp = tikToe;
         if (box4.innerText === "X") {
-            box4.style.color = "yellow"
+            box4.style.color = "#ff0000"
         }
         if (box4.innerText === "O") {
-            box4.style.color = "red"
+            box4.style.color = "#07215f"
         }
         kik();
     }
@@ -296,11 +428,17 @@ box5.addEventListener('click', () => {
     else {
         box5.innerHTML = `<div class="active disable">${tikToe}</div>`;
         temp = tikToe;
+
+        // setTimeout(()=>{
+        //     box1.innerHTML =`<div class="active disable">O</div>`;
+        // }, 1000)
+
+
         if (box5.innerText === "X") {
-            box5.style.color = "yellow"
+            box5.style.color = "#ff0000"
         }
         if (box5.innerText === "O") {
-            box5.style.color = "red"
+            box5.style.color = "#07215f"
         }
         kik();
     }
@@ -317,11 +455,14 @@ box6.addEventListener('click', () => {
     else {
         box6.innerHTML = `<div class="active disable">${tikToe}</div>`;
         temp = tikToe;
+        // setTimeout(()=>{
+        //     box5.innerHTML =`<div class="active disable">O</div>`;
+        // }, 1000)
         if (box6.innerText === "X") {
-            box6.style.color = "yellow"
+            box6.style.color = "#ff0000"
         }
         if (box6.innerText === "O") {
-            box6.style.color = "red"
+            box6.style.color = "#07215f"
         }
         kik();
     }
@@ -339,10 +480,10 @@ box7.addEventListener('click', () => {
         box7.innerHTML = `<div class="active disable">${tikToe}</div>`;
         temp = tikToe;
         if (box7.innerText === "X") {
-            box7.style.color = "yellow"
+            box7.style.color = "#ff0000"
         }
         if (box7.innerText === "O") {
-            box7.style.color = "red"
+            box7.style.color = "#07215f"
         }
         kik();
     }
@@ -360,10 +501,10 @@ box8.addEventListener('click', () => {
         box8.innerHTML = `<div class="active disable">${tikToe}</div>`;
         temp = tikToe;
         if (box8.innerText === "X") {
-            box8.style.color = "yellow"
+            box8.style.color = "#ff0000"
         }
         if (box8.innerText === "O") {
-            box8.style.color = "red"
+            box8.style.color = "#07215f"
         }
         kik();
     }
@@ -381,10 +522,10 @@ box9.addEventListener('click', () => {
         box9.innerHTML = `<div class="active disable">${tikToe}</div>`;
         temp = tikToe;
         if (box9.innerText === "X") {
-            box9.style.color = "yellow"
+            box9.style.color = "#ff0000"
         }
         if (box9.innerText === "O") {
-            box9.style.color = "red"
+            box9.style.color = "#07215f"
         }
         kik();
     }
